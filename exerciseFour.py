90e8bb7d04dacfbbd4536b24f94df85e77a2e1c3
Andrew <andrewkjar34@gmail.com>
2020-03-18 12:57:44 -0700

    updated exercise four with join function

--- exerciseFour.py
+++ exerciseFour.py
@@ -1,4 +1,4 @@
-# Exercise Four (Split and Join)
+# Exercise Four (Split and Join or Replace)
 
 # From the list above fill a list(friends_list) properly
 # with the names of all the friends. One per "slot"
@@ -8,6 +8,12 @@
 csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
 friends_list = ['Exercise: fill me with names']
 
+# Split and Join
+friends_list2 = ','.join(','.join(csv.split(';')).split(':')).split(',')
+
+print(friends_list2)
+
+# Split and Replace
 csv = csv.replace(':', ',').replace(';', ',').split(',')
 friends_list = csv
 
